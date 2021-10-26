9e1cf9c0255ce69769c388af880278acf487e48f
Beibei Zhu <321206595@qq.com>
2021-10-26 10:25:38 +0800

    Initialize project

---
+++ TestCircleCI/TestCircleCI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCircleCI
+//
+//  Created by Beibei Zhu on 2021/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
